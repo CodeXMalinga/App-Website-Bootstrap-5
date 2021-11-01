5482857ccaa4c75944c3c82e1cb5b10ee67953ba
CodeXMalinga <rdimalingadesilva@gmail.com>
2021-11-01 23:07:24 +0530

    init

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,30 +50,30 @@ document.addEventListener("DOMContentLoaded", () => {
     counter("count4", 0, 10, 2000);
  });
 
-//  // APP-SCREEN
-//  var swiper = new Swiper( '.swiper-container.app-screen', {
-//     effect: 'coverflow',
-//     loop: true,
-//     autoplaySpeed: 3000,
-//     centeredSlides: true,
-//     slidesPerView: 'auto',
-//     autoplay: {
-//         delay: 5000,
-//         disableOnInteraction: false
-//     },
-//     pagination: {
-//         el: '.swiper-pagination',
-//         clickable: true,
-//     },
-//     paginationClickable: true,
-//     coverflow: {
-//         rotate: 0,
-//         stretch: 100,
-//         depth: 150,
-//         modifier: 1.5,
-//         slideShadows : false,
-//     }
-// });  
+ // APP-SCREEN
+ var swiper = new Swiper( '.swiper-container.app-screen', {
+    effect: 'coverflow',
+    loop: true,
+    autoplaySpeed: 3000,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    paginationClickable: true,
+    coverflow: {
+        rotate: 0,
+        stretch: 100,
+        depth: 150,
+        modifier: 1.5,
+        slideShadows : false,
+    }
+});  
 
 // // Side Navigation Bar Close While We click On Navigation Links
 
